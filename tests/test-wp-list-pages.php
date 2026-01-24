194c4b4020a122cde3902592da01fe9074916551
Nikhil <nikhil.chavan@automattic.com>
2026-01-24 20:12:25 +0530

    fix: Update assertions for PHPUnit 9 string methods
    - Replace assertContains/assertNotContains with assertStringContainsString
      and assertStringNotContainsString for string comparisons
    - Add .phpunit.result.cache to .gitignore

--- tests/test-wp-list-pages.php
+++ tests/test-wp-list-pages.php
@@ -63,7 +63,7 @@ class TestWPListPagesUnlisted extends WP_UnitTestCase {
 		);
 
 		// Assert that the Unlisted Post is visible in wp_list_page().
-		$this->assertNotContains( 'Unlisted Post Title', $list_pages_markup );
+		$this->assertStringNotContainsString( 'Unlisted Post Title', $list_pages_markup );
 	}
 
 	/**
@@ -99,6 +99,6 @@ class TestWPListPagesUnlisted extends WP_UnitTestCase {
 		);
 
 		// Assert that the Listed Post is visible in wp_list_page().
-		$this->assertContains( 'Listed Post Title', $list_pages_markup );
+		$this->assertStringContainsString( 'Listed Post Title', $list_pages_markup );
 	}
 }
